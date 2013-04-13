3ed32ae1239c0334e967f1553f9fa247129af0c8
Darescu Ionut <johnny@esi.ro>
2013-04-13 12:49:03 +0300

    mm-video: Use actual kernel headers

--- mm-video/vidc/venc/Android.mk
+++ mm-video/vidc/venc/Android.mk
@@ -55,6 +55,7 @@ include $(CLEAR_VARS)
 libmm-venc-inc      := bionic/libc/include
 libmm-venc-inc      += bionic/libstdc++/include
 libmm-venc-inc      += $(LOCAL_PATH)/inc
+libmm-venc-inc      += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
 libmm-venc-inc      += $(OMX_VIDEO_PATH)/vidc/common/inc
 libmm-venc-inc      += hardware/qcom/media-legacy/mm-core/inc
 #libmm-venc-inc      += bionic/libc/kernel/common/linux
@@ -93,6 +94,7 @@ include $(CLEAR_VARS)
 
 mm-venc-test720p-inc            := $(TARGET_OUT_HEADERS)/mm-core
 mm-venc-test720p-inc            += $(LOCAL_PATH)/inc
+mm-venc-test720p-inc            += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
 mm-venc-test720p-inc            += $(OMX_VIDEO_PATH)/vidc/common/inc
 mm-venc-test720p-inc            += hardware/qcom/media-legacy/mm-core/inc
 mm-venc-test720p-inc            += hardware/qcom/display-legacy/libgralloc
@@ -119,6 +121,7 @@ include $(BUILD_EXECUTABLE)
 include $(CLEAR_VARS)
 
 venc-test-inc                   += $(LOCAL_PATH)/inc
+venc-test-inc                   += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
 venc-test-inc                   += hardware/qcom/display-legacy/libgralloc
 
 LOCAL_MODULE                    := mm-video-encdrv-test
